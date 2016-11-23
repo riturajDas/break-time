17ebe36be07834dc73bd2c6b9f09390195e572ac
slayerOne <rituraj.das8@gmail.com>
2016-11-23 22:28:38 +0530

    Gold = Kenny G :D

--- break_time.py
+++ break_time.py
@@ -1,5 +1,5 @@
 #this program alerts that 2 hours have passed since staring at the computer
-#screen and its now time to take a break by watching an youtube video
+#screen and its now time to take a break with a soothing youtube video
 import time
 import webbrowser
 
@@ -9,5 +9,6 @@ total_counts = 0
 print "PROGRAM STARTED ON: "+time.ctime()
 while(total_breaks > total_counts):
     time.sleep(2*60*60)
-    webbrowser.open("https://www.youtube.com/watch?v=M8YNhIpdzQ0")
+    print "BREAK TIME #"+total_breaks+" !!!"
+    webbrowser.open("https://www.youtube.com/watch?v=MV4hrOBC2zc&list=UULP1LFCaIifzrpyUX2o0xiA")
     total_breaks += 1
